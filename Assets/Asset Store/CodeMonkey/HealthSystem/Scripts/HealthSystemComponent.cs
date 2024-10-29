4a014fa69df351e2ef479567bfc571ecd4c8a7df
psc-utb <psc.utb@gmail.com>
2024-10-29 11:38:51 +0100

    Health system with Health bar
    + Health system asset from CodeMonkey added to the project
    + HealthSystemComponent added to the Knight and Dragon game objects
    ~ Attacking script improved -> damage is randomly generated, numbers are shown and fly away
    + Mouth with attacking script was added to the dragon
    + TextMeshPro package installed

---
+++ Assets/Asset Store/CodeMonkey/HealthSystem/Scripts/HealthSystemComponent.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace CodeMonkey.HealthSystemCM {
+
+    /// <summary>
+    /// Adds a HealthSystem to a Game Object
+    /// </summary>
+    public class HealthSystemComponent : MonoBehaviour, IGetHealthSystem {
+
+        [SerializeField]
+        private HealthSystem healthSystem;
+
+
+        private void Awake() {
+            
+        }
+
+        /// <summary>
+        /// Get the Health System created by this Component
+        /// </summary>
+        public HealthSystem GetHealthSystem() {
+            return healthSystem;
+        }
+
+
+    }
+
+}
--- Assets/Scripts/Attacking.cs
+++ Assets/Scripts/Attacking.cs
@@ -1,11 +1,42 @@
+using CodeMonkey.HealthSystemCM;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Attacking : MonoBehaviour
 {
+    [SerializeField]
+    int damage_min = 1;
+    [SerializeField]
+    int damage_max = 5;
+
+    [SerializeField]
+    GameObject numberPrefab;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Attacked");
+        var character = collision.gameObject.GetComponent<HealthSystemComponent>();
+
+        if (character != null)
+        {
+            int damage = Random.Range(damage_min, damage_max + 1);
+            character.GetHealthSystem().Damage(damage);
+            //Debug.Log("Attacked");
+            if(numberPrefab != null)
+            {
+                var go = Instantiate(numberPrefab);
+
+                go.transform.position = new Vector2(character.transform.position.x, go.transform.position.y);
+
+                TextMeshPro textMeshPro = go.GetComponent<TextMeshPro>();
+                textMeshPro.text = "-" + damage;
+
+                Rigidbody2D rb2D = go.GetComponent<Rigidbody2D>();
+                rb2D.velocity = new Vector2(0, 2);
+
+                Destroy(go, 3);
+            }
+        }
     }
 }
