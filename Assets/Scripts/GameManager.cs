6e24e2709e4bb4293de45dbd57c2c2343225e024
psc-utb <psc.utb@gmail.com>
2024-11-19 09:47:13 +0100

    MainMenu activation/deactivation
    MainMenu can be activated now during the game.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -23,4 +23,51 @@ public class GameManager : MonoBehaviour
             SceneManager.LoadScene(sceneName);
         }
     }
+
+
+    [SerializeField]
+    bool mainMenuCanBeDisplayed = false;
+    [SerializeField]
+    private string mainMenuSceneName;
+
+    AudioListener audioListenerInScene;
+
+    // set things up here
+    void Awake()
+    {
+        audioListenerInScene = FindObjectOfType<AudioListener>();
+    }
+
+    // game loop
+    void Update()
+    {
+        if (mainMenuCanBeDisplayed == true && Input.GetButtonDown("Menu"))
+        {
+            if (Time.timeScale > 0f)
+            {
+                audioListenerInScene.enabled = false;
+                SceneManager.LoadScene(mainMenuSceneName, LoadSceneMode.Additive);
+                Time.timeScale = 0f;
+            }
+            else
+            {
+                AsyncOperation asyncOpUnloadScene = SceneManager.UnloadSceneAsync(mainMenuSceneName);
+                asyncOpUnloadScene.completed += UnloadScene_completed;
+                Time.timeScale = 1f;
+            }
+        }
+    }
+
+    private void UnloadScene_completed(AsyncOperation obj)
+    {
+        if (audioListenerInScene != null)
+            audioListenerInScene.enabled = true;
+    }
+
+    private void OnDestroy()
+    {
+        Time.timeScale = 1f;
+        if (audioListenerInScene != null)
+            audioListenerInScene.enabled = true;
+    }
 }
